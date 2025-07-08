9d615e78f51dbb4d13c1ffe30c3bccc13dec3309
Goo Zong Lin <zonglingoo@gmail.com>
2025-07-08 13:20:21 +0800

    project setup
    React, Typescript, Vite, Tailwind CSS

---
+++ collabevent/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-500">
+      <h1 className="text-5xl font-bold text-white">Hello World</h1>
+    </div>
+  )
+}
+
+export default App
